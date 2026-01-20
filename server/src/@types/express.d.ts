1cb30e1425f976caf4084feb412443e6684ee8fe
thiagoeu <araujo.thiago1051@gmail.com>
2026-01-20 16:53:29 -0300

    fix: ajuste de caminho de arquivo

--- server/src/@types/express.d.ts
+++ server/src/@types/express.d.ts
@@ -1,4 +1,4 @@
-import { JwtPayloadUser } from '../auth/types/jwt-payload-user.type';
+import { JwtPayloadUser } from '../shared/interfaces/jwt-payload-user.type';
 
 declare global {
   namespace Express {
